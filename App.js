3386f301af53553119ac9643562db3daa155d3d0
WahajAhmedIqbal <wahaj465@gmail.com>
2020-11-23 19:16:45 +0500

    navgation

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
-import { createStackNavigator, createAppContainer } from "react-navigation";
-import SearchScreen from "./src/Screens/SearchScreen";
+// import { createStackNavigator, createAppContainer } from "react-navigation";
+// import SearchScreen from "./src/Screens/SearchScreen";
 
-const navigator = createStackNavigator(
-  {
-    Search: SearchScreen,
-  },
-  {
-    initialRouteName: "Search",
-    defaultNavigationOptions: {
-      title: "Business Search",
-    },
-  }
-);
-export default createAppContainer(navigator);
+// const navigator = createStackNavigator(
+//   {
+//     Search: SearchScreen,
+//   },
+//   {
+//     initialRouteName: "Search",
+//     defaultNavigationOptions: {
+//       title: "Business Search",
+//     },
+//   }
+// );
+// export default createAppContainer(navigator);
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+const App = () => {
+  return (
+    <View>
+      <Text> hello world</Text>
+    </View>
+  );
+};
+
+export default App;
